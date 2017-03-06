e91bc3a4d88f139c21599d35ef7e567ccdeaa528
bachen <otori@qq.com>
2017-03-07 00:02:37 +0800

    UI test complete

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Part2: API Test based on requests(Python)
 
 # UI_test.py:
 
+Preparation:
+
+1) open Develop option of Safari;
+
+2) Safari -> Preference -> Tab -> choose 'always open on tab'.
+
 Automate the test case "Purchase book at Amazon website" based on Selenium Webdriver
 "Purchase book at Amazon website" 
 
@@ -17,4 +23,4 @@ And test case steps:
 3. Click "软件测试（原书第2版）"
 4. Click "加入购物车" at “软件测试（原书第2版）” page
 5. Assert that text "商品已加入购物车" appears 
-6. Assert that book price is "20.40"
+6. Assert that book price is "20.40" (2017.03.06 the price of book is 24.50)
--- UI_utils.py
+++ UI_utils.py
@@ -1,1 +1,69 @@
+from selenium.common.exceptions import *
+from selenium import webdriver
+import time
 
+
+def open_url(self, url):
+    dr = self.dr
+    try:
+        dr.get(url)
+        dr.maximize_window()
+    except:
+        print "Fail to open url: %s" % url
+    finally:
+        time.sleep(5)
+
+
+def click(self, xpath):
+    dr = self.dr
+    try:
+        em = dr.find_element_by_xpath(xpath)
+        em.click()
+    except NoSuchElementException:
+        print "There is no element: %s" % xpath
+    except RemoteDriverServerException:
+        print "Remote driver server exception."
+    except:
+        print "Other exception."
+    else:
+        print "Click element %s successfully." % xpath
+    finally:
+        time.sleep(5)
+
+
+def find(self, xpath):
+    dr = self.dr
+    try:
+        em = dr.find_element_by_xpath(xpath)
+        return em
+    except NoSuchElementException:
+        print "There is no element: %s" % xpath
+    except:
+        print "Other exception."
+    finally:
+        time.sleep(5)
+
+
+def finds(self, xpath):
+    dr = self.dr
+    try:
+        ems = dr.find_elements_by_xpath(xpath)
+        return ems
+    except NoSuchElementException:
+        print "There is no element: %s" % xpath
+    except:
+        print "Other exception."
+    finally:
+        time.sleep(5)
+
+
+def inputs(self, xpath, msg):
+    dr = self.dr
+    try:
+        dr.find_element_by_xpath(xpath).send_keys(msg)
+    except:
+        print "Fail to input words: %s to element: %s" % (msg, xpath)
+    else:
+        print "Input words '%s' successfully." % msg
+    finally:
+        time.sleep(5)
