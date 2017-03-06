6c14eaf1640c7b7264890db1e655e9671e508bae
kasanagimotoko <otori@qq.com>
2017-03-06 13:32:07 +0800

    test method modify

--- UI_test.py
+++ UI_test.py
@@ -1,8 +1,10 @@
 # coding=utf-8
+import time
+import unittest2
 from selenium import webdriver
-import unittest,time
 
-class price(unittest.TestCase):
+
+class price(unittest2.TestCase):
     def setUp(self):
         self.dr = webdriver.Firefox()
         self.dr.implicitly_wait(30)
@@ -16,27 +18,22 @@ class price(unittest.TestCase):
         time.sleep(5)
         dr.find_element_by_xpath(search_btn_xpath).click()
         time.sleep(5)
-        title = u"软件测试(原书第2版)"
-        ems = dr.find_elements_by_xpath(u"//ul/li/div/div[3]/div[1]/a")
-        count = 0
-        for em in ems:
-            count += 1
-            if em.get_attribute(u"title") == title:
-                break
-        add_to_cart_xpath = u"//ul/li["+ str(count) + u"]/div/div[7]/form[1]/div/span/span/input"
-        dr.find_element_by_xpath(add_to_cart_xpath).click()
+        target_book_xpath = u"//a/h2[@data-attribute='软件测试(原书第2版)']"
+        current_window_handle = dr.current_window_handle
+        dr.find_element_by_xpath(target_book_xpath).click()
         time.sleep(5)
-        # switch window
+        window_handles = dr.window_handles
+        for window_handle in window_handles:
+            if window_handle != current_window_handle:
+                dr.switch_to.window(window_handle)
+        add_to_xpath = u""
+        dr.find_element_by_xpath(add_to_xpath).click()
+        time.sleep(5)
+        # assert
         confirm_text_xpath = u"//div[@id='huc-v2-order-row-confirm-text']/h1"
-        if u"商品已加入购物车" in dr.find_element_by_xpath(confirm_text_xpath).text:
-            assert True
-        else:
-            assert False
+        self.assertIn(u"商品已加入购物车", dr.find_element_by_xpath(confirm_text_xpath).text, "fail to judge")
         price_xpath = u"//div[@id='hlb-subcart']/div/span/span[@class='a-color-price hlb-price a-inline-block a-text-bold']"
-        if u"20.90" in dr.find_element_by_xpath(price_xpath).text:
-            assert True
-        else:
-            assert False
+        self.assertIn(u"20.90", dr.find_element_by_xpath(price_xpath).text, "fail to judge")
 
     def tearDown(self):
         self.dr.quit()
